040f819893aa59d9ef075374482d2e1d02380f28
laurent <htd.laurent@gmail.com>
2016-12-09 15:52:17 +0100

    new Makefile + make.py python3

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 
 
 all:
-	python	bin/make.py
+	@echo "Compiling lib_smart-building..."
+	@python	bin/make.py
+	@echo "\n\033[0;32m[OK] Compiling lib_smart-building : Done\033[m\n"
 
 clean:
-	rm -rf .gen/ dist/*.js models.js processes.js is-sim.js;
+	@! test -e .gen || rm -rf .gen/ dist/*.js models.js processes.js is-sim.js;
+	@echo "\033[0;32m[OK] cleaning lib_smart-building : Done\033[m"
 
 .PHONY: all clean
--- bin/make.py
+++ bin/make.py
@@ -1,29 +1,31 @@
 from concat_js import *
 
-exec_cmd( "echo \"\" > models.js " )
-exec_cmd( "echo \"\" > processes.js " )
-exec_cmd( "mkdir -p dist " )
+exec_cmd_silent( "echo \"\" > models.js " )
+exec_cmd_silent( "echo \"\" > processes.js " )
+exec_cmd_silent( "mkdir -p dist " )
 
 
 for p in os.listdir( "modules" ):
-    exec_cmd( "echo \"\" > dist/" + p + ".js " )
+    exec_cmd_silent( "echo \"\" > dist/" + p + ".js " )
     ##########
     # models #
     ##########
-    concat_js( "modules/" + p + "/models", "dist/models." + p + ".js" )
-    if os.path.isfile( "dist/models." + p + ".js" ):
-      exec_cmd( "cat dist/models." + p + ".js >> models.js" )
-      exec_cmd( "cat dist/models." + p + ".js >> dist/" + p + ".js " )
+    if os.path.isfile(  "modules/" + p + "/models" ):
+        concat_js( "modules/" + p + "/models", "dist/models." + p + ".js" )
+        if os.path.isfile( "dist/models." + p + ".js" ):
+            exec_cmd_silent( "cat dist/models." + p + ".js >> models.js" )
+            exec_cmd_silent( "cat dist/models." + p + ".js >> dist/" + p + ".js " )
     #############
     # processes #
     #############
-    concat_js( "modules/" + p + "/processes", "dist/processes." + p + ".js" )
-    if os.path.isfile( "dist/processes." + p + ".js" ):
-      exec_cmd( "cat dist/processes." + p + ".js >> processes.js" )
-      exec_cmd( "cat dist/processes." + p + ".js >> dist/" + p + ".js " )
+    if os.path.isfile( "modules/" + p + "/processes" ):
+        concat_js( "modules/" + p + "/processes", "dist/processes." + p + ".js" )
+        if os.path.isfile( "dist/processes." + p + ".js" ):
+            exec_cmd_silent( "cat dist/processes." + p + ".js >> processes.js" )
+            exec_cmd_silent( "cat dist/processes." + p + ".js >> dist/" + p + ".js " )
 
 ##########
 # is-sim #
 ##########
 concat_js( "modules/", "is-sim.js" )
-exec_cmd( "cat config.js >> is-sim.js " )
+exec_cmd_silent( "cat config.js >> is-sim.js " )
